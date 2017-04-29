0b25255e8da4223389b904ec8380a6beee4b49dc
Alicia Cunningham <aliciacunningham@aliciacmacbook2.bigpond>
2017-04-29 11:22:21 +1000

    Initial Commit

---
+++ Swift App (Practice)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App (Practice)
+//
+//  Created by Alicia Cunningham on 29/04/2017.
+//  Copyright © 2017 Alicia Cunningham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
